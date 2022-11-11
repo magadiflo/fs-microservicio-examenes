561b078f723b20c3891ced6c1337c3ce0648638d
Martín <magadiflo@gmail.com>
2022-11-11 18:35:37 -0500

    Proyecto base - Con dependencias: MySQL Driver, Spring Data JPA, Spring Web, Cliente Eureka

---
+++ src/main/java/com/magadiflo/examenes/app/FsMicroservicioExamenesApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.examenes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsMicroservicioExamenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsMicroservicioExamenesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magadiflo/examenes/app/FsMicroservicioExamenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo.examenes.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsMicroservicioExamenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
