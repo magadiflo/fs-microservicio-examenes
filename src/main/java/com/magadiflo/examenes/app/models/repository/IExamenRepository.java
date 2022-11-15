b47ac4c682d3e4f234935bde356bf5e96aee2fba
Martín <magadiflo@gmail.com>
2022-11-14 22:59:33 -0500

    Añadiendo paginación

--- src/main/java/com/magadiflo/examenes/app/models/repository/IExamenRepository.java
+++ src/main/java/com/magadiflo/examenes/app/models/repository/IExamenRepository.java
@@ -3,10 +3,11 @@ package com.magadiflo.examenes.app.models.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 import com.magadiflo.commons.examenes.models.entity.Examen;
 
-public interface IExamenRepository extends CrudRepository<Examen, Long> {
+public interface IExamenRepository extends PagingAndSortingRepository<Examen, Long> {
 
 	@Query("SELECT e FROM Examen AS e WHERE e.nombre LIKE %?1%")
 	List<Examen> findByNombre(String termino);
